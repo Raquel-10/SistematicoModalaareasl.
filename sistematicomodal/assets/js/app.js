2216ce9c2399288e02cc0839f269816de38cc444
Raquel-10 <31806912+Raquel-10@users.noreply.github.com>
2022-05-14 17:21:49 -0600

    Add files via upload

---
+++ sistematicomodal/assets/js/app.js
@@ -0,0 +1,35 @@
+function InsertarRegistro(){
+    if (ValidarCedula(document.getElementById('IDTextBox').value)){
+        var a=document.getElementById('PersonTable').insertRow(1);
+        var b=a.insertCell(0);
+        var c=a.insertCell(1);
+        var d=a.insertCell(2);
+        var e=a.insertCell(3);
+        var f=a.insertCell(4);
+        var g=a.insertCell(5);
+        var h=a.insertCell(6);
+        var i=a.insertCell(7)
+        b.innerHTML=document.getElementById('IDTextBox').value;
+        c.innerHTML=document.getElementById('FirstNameTextBox').value;
+        d.innerHTML=document.getElementById('LastNameTextBox').value;
+        //
+        var genero = GetCheckedRadioValue("Sexo");
+        e.innerHTML=genero;     
+        //
+        var pasatiempos= GetCheckedBoxesValue("Pasatiempos");  
+        f.innerHTML=pasatiempos;
+        var buttons ="<button name='EditButton' onclick='ShowModalForEdit(this)'>Editar</button>";
+        buttons = buttons.concat("&nbsp;").concat("<button name='DeleteButton' onclick='deleteRow(this)'>Eliminar</button>");
+        i.innerHTML=buttons;
+        g.innerHTML=document.getElementByID('Profesiones').value;
+        h.innerHTML= document.getElementById('HijosTextBox').value;
+        var modal = document.getElementById('myModal');
+        modal.style.display = "none";
+    }
+
+}
+
+function deleteRow(sender) {
+    var i = sender.parentNode.parentNode.rowIndex;
+    document.getElementById("PersonTable").deleteRow(i);
+}
